88b08606c7f4ee7527e1474f6f9bcd246078f2c2
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-09 12:05:42 +0900

    Compiler program uses parser

--- Compiler/src/Program.java
+++ Compiler/src/Program.java
@@ -1,65 +1,23 @@
-import Rpc.Compiler.*;
+import java.io.FileInputStream;
+
+import Compiler.*;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        /*
-        Rpc.Compiler.Rpc.Compiler compiler = new Rpc.Compiler.Rpc.Compiler(new FileInputStream("interface.idl"));
-        */
-
-        Interface exampleInterface;
-        Operation operation;
-        Parameter parameter;
-
-        exampleInterface = new Interface();
-        exampleInterface.setName("RemoteInterface");
-
-        // ---- someLocalOperation
-        operation = new Operation();
-        operation.setName("someLocalOperation");
-        operation.setLocality("local");
-        operation.setType("void");
-
-        parameter = new Parameter();
-        parameter.setRef(true);
-        parameter.setIn(false);
-        parameter.setOut(false);
-        parameter.setType("int");
-        parameter.setName("arg0");
-
-        operation.addParameter(parameter);
-
-        parameter = new Parameter();
-        parameter.setRef(false);
-        parameter.setIn(true);
-        parameter.setOut(true);
-        parameter.setType("string");
-        parameter.setName("arg1");
-
-        operation.addParameter(parameter);
-        exampleInterface.addOperation(operation);
-
-
-        // ---- someRemoteOperation
-        operation = new Operation();
-        operation.setName("someRemoteOperation");
-        operation.setLocality("remote");
-        operation.setType("int");
-
-        exampleInterface.addOperation(operation);
+        IdlCompiler compiler = new IdlCompiler(new FileInputStream("interface.idl"));
+        Interface interface_ = compiler.getInterface();
 
         System.out.println("Parsed as:");
-        new InterfaceEmitter(System.out).emit(exampleInterface);
+        new InterfaceEmitter(System.out).emit(interface_);
 
         System.out.print("\n--------------------------------\n");
 
-        System.out.println("Java Interface");
-        new JavaInterfaceEmitter(System.out).emit(exampleInterface);
+        System.out.println("Java Interface:");
+        new JavaInterfaceEmitter(System.out).emit(interface_);
 
         System.out.print("\n--------------------------------\n");
 
-        System.out.println("Java Stub");
-        new JavaStubEmitter(System.out).emit(exampleInterface);
-
-
+        System.out.println("Java Stub:");
+        new JavaStubEmitter(System.out).emit(interface_);
     }
 }
