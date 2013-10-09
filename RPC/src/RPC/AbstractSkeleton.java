f51efe28e61550288ac37eed0468ff4ea8febc2c
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-09 12:05:15 +0900

    Introduce stub and skeleton base classes

--- RPC/src/RPC/AbstractSkeleton.java
+++ RPC/src/RPC/AbstractSkeleton.java
@@ -1,11 +1,4 @@
 package Rpc;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Naav
- * Date: 08/10/13
- * Time: 12:19
- * To change this template use File | Settings | File Templates.
- */
-public class AbstractSkeleton {
+public abstract class AbstractSkeleton {
 }
