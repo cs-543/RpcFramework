349bdf01626e8ac3d75bcaa4b317c1243a802347
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-09 12:21:48 +0900

    Comment Tokenizer

--- Compiler/src/Compiler/Tokenizer.java
+++ Compiler/src/Compiler/Tokenizer.java
@@ -1,92 +1,163 @@
 package Compiler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.CharBuffer;
 
+import static Compiler.Token.TokenType.*;
+
+/**
+ * Creates Tokens from a stream of characters.
+ */
 public class Tokenizer {
-    private CharBuffer buffer = CharBuffer.allocate(4096);
+    /**
+     * Map of single letter tokens and their corresponding TokenType.
+     */
+    private static final Map<Character, Token.TokenType> simpleTokens = new HashMap<Character, Token.TokenType>();
+
+    /**
+     * Map of keywords and their corresponding TokenType.
+     */
+    private static final Map<String, Token.TokenType> keywords = new HashMap<String, Token.TokenType>();
+
+    /**
+     * Populate both maps.
+     */
+    static
+    {
+        simpleTokens.put('{', TT_OPENBRACE);
+        simpleTokens.put('}', TT_CLOSEBRACE);
+        simpleTokens.put('[', TT_OPENBRACKET);
+        simpleTokens.put(']', TT_CLOSEBRACKET);
+        simpleTokens.put('(', TT_OPENPARENS);
+        simpleTokens.put(')', TT_CLOSEPARENS);
+        simpleTokens.put(',', TT_COMMA);
+        simpleTokens.put(';', TT_SEMICOLON);
+
+        keywords.put("interface",   TT_INTERFACE);
+        keywords.put("AtMostOnce" , TT_ATMOSTONCE);
+        keywords.put("AtLeastOnce", TT_ATLEASTONCE);
+        keywords.put("async",       TT_ASYNC);
+        keywords.put("local",       TT_LOCAL);
+        keywords.put("in",          TT_IN);
+        keywords.put("out",         TT_OUT);
+    }
+
+    /**
+     * Input stream reader from which this Tokenizer reads.
+     */
     private InputStreamReader reader;
-    private boolean lastBuffer;
 
-    private int column;
-    private int line;
+    /**
+     * Character buffer, for performance.
+     */
+    private char[] buffer = new char[4096];
 
-    private StringBuilder wordBuilder;
+    /**
+     * Current position in buffer.
+     */
+    private int bufferPosition;
 
+    /**
+     * Number of characters in buffer.
+     */
+    private int inBufferCount;
+
+    /**
+     * Current column in file.
+     */
+    private int column = 1;
+
+    /**
+     * Current line in file.
+     */
+    private int line = 1;
+
+    /**
+     * Initializes a new instance of the Tokenizer class.
+     * @param stream The stream to tokenize.
+     */
     public Tokenizer(InputStream stream) {
         reader = new InputStreamReader(stream);
     }
 
+    /**
+     * @return The next available Token, or null if end of stream was reached.
+     * @throws Exception
+     */
     public Token nextToken() throws Exception {
         char c = nextNonWhitespace();
 
-        switch (c) {
-            // Open brace.
-            case '{':
-                return makeToken(Compiler.Token.TokenType.TT_OPENBRACE);
-            case '}':
-                return makeToken(Compiler.Token.TokenType.TT_CLOSEBRACE);
-            case '[':
-                return makeToken(Compiler.Token.TokenType.TT_OPENBRACKET);
-            case ']':
-                return makeToken(Compiler.Token.TokenType.TT_CLOSEBRACKET);
-            case '(':
-                return makeToken(Compiler.Token.TokenType.TT_OPENPARENS);
-            case ')':
-                return makeToken(Compiler.Token.TokenType.TT_CLOSEPARENS);
-            case ',':
-                return makeToken(Compiler.Token.TokenType.TT_COMMA);
-            case ';':
-                return makeToken(Compiler.Token.TokenType.TT_SEMICOLON);
-            default:
-                return parseWord(c);
+        // End of file reached.
+        if (c == '\0') {
+            return null;
         }
+
+        // Simple token.
+        if (simpleTokens.containsKey(c)) {
+            return makeToken(simpleTokens.get(c));
+        }
+
+        // Keyword or identifier start.
+        if (Character.isAlphabetic(c)) {
+            return parseWord(c);
+        }
+
+        throw new Error("Parse error line " + line + ", unexpected `" + c + "`");
     }
 
-    private Token makeToken(Compiler.Token.TokenType type) {
+    /**
+     * Builds a Token of a given type.
+     * @param type The type of the Token to create.
+     * @return The newly created Token.
+     */
+    private Token makeToken(Token.TokenType type) {
         return new Token(line, column, type);
     }
 
+    /**
+     * Parses a word (keyword or identifier)
+     * @param initial First character of the word.
+     * @return The parsed word.
+     * @throws Exception
+     */
     private Token parseWord(char initial) throws Exception {
-        wordBuilder = new StringBuilder();
+        StringBuilder wordBuilder = new StringBuilder();
 
+        // Read all alphanumeric characters into the word builder.
         char c = initial;
         do {
             wordBuilder.append(c);
             c = nextChar();
-        } while (Character.isAlphabetic(c));
+        } while (Character.isAlphabetic(c) || Character.isDigit(c));
 
-        // Put the last read character back
-        buffer.put(c);
+        // Put the last read character back because we read one too much.
+        bufferPosition--;
         column--;
 
         String word = wordBuilder.toString();
-        switch (word) {
-            case "interface":
-                return makeToken(Compiler.Token.TokenType.TT_INTERFACE);
-            case "AtMostOnce":
-                return makeToken(Compiler.Token.TokenType.TT_ATMOSTONCE);
-            case "AtLeastOnce":
-                return makeToken(Compiler.Token.TokenType.TT_ATLEASTONCE);
-            case "async":
-                return makeToken(Compiler.Token.TokenType.TT_ASYNC);
-            case "local":
-                return makeToken(Compiler.Token.TokenType.TT_LOCAL);
-            case "in":
-                return makeToken(Compiler.Token.TokenType.TT_IN);
-            case "out":
-                return makeToken(Compiler.Token.TokenType.TT_OUT);
-            default:
-                return new Token(line, column, Compiler.Token.TokenType.TT_IDENTIFIER, word);
+
+        // Keyword.
+        if (keywords.containsKey(word)) {
+            return makeToken(keywords.get(word));
         }
+
+        // Identifier.
+        return new Token(line, column, TT_IDENTIFIER, word);
     }
 
+    /**
+     * @return The next non whitespace character in the stream.
+     * @throws Exception
+     */
     private char nextNonWhitespace() throws Exception {
         char c = nextChar();
 
         while (Character.isWhitespace(c)) {
             if (c == '\n') {
+                // Increase line count and reset column caret.
                 line++;
                 column = 0;
             }
@@ -97,21 +168,16 @@ public class Tokenizer {
         return c;
     }
 
+    /**
+     * @return The next character in the stream.
+     * @throws Exception
+     */
     private char nextChar() throws Exception {
-        column++;
-
-        if (!buffer.hasRemaining()) {
-            int read = reader.read(buffer);
-
-            if (read < buffer.capacity()) {
-                lastBuffer = true;
-            }
+        if (bufferPosition == inBufferCount) {
+            inBufferCount = reader.read(buffer);
         }
 
-        return buffer.get();
-    }
-
-    private boolean endOfFile() {
-        return lastBuffer && buffer.remaining() > 0;
+        column++;
+        return buffer[bufferPosition++];
     }
 }
