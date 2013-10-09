5d88539dc1daf8b7db9b40a07f0f55993eaaf840
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-09 12:04:50 +0900

    Introduce Out and InOut parameter wrappers

--- RPC/src/RPC/InOut.java
+++ RPC/src/RPC/InOut.java
@@ -1,11 +1,5 @@
 package Rpc;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Naav
- * Date: 08/10/13
- * Time: 12:18
- * To change this template use File | Settings | File Templates.
- */
-public class InOut {
+public class InOut<T> {
+    T value;
 }
--- RPC/src/RPC/Out.java
+++ RPC/src/RPC/Out.java
@@ -1,11 +1,7 @@
 package Rpc;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Naav
- * Date: 08/10/13
- * Time: 12:17
- * To change this template use File | Settings | File Templates.
- */
-public class Out {
+public class Out<T> {
+    T value;
+
+
 }
