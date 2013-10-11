93ae3b3787ecaba4f83e7f4eb2678906c24544bd
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-12 00:24:24 +0900

    Fix build

--- Rpc/src/Rpc/AbstractStub.java
+++ Rpc/src/Rpc/AbstractStub.java
@@ -1,12 +1,11 @@
 package Rpc;
 
 public abstract class AbstractStub {
-    public <T> T invoke(String methodName, Object... arguments) {
-
+    public <T> T invoke(String methodName, Object... arguments) throws Exception {
+        throw new Exception("Not implemented");
     }
 
-    public <T> void invokeAsync(Callback<T> callback, String methodName, Object... arguments) {
-
-
+    public <T> void invokeAsync(Callback<T> callback, String methodName, Object... arguments) throws Exception {
+        throw new Exception("Not implemented");
     }
 }
--- RpcRegistry/src/Program.java
+++ RpcRegistry/src/Program.java
@@ -1,13 +1,5 @@
 
 public class Program {
     public static void main(String args[]) {
-        if (args.length == 0) {
-            printUsage();
-            return;
-        }
-    }
-
-    private static void printUsage() {
-        System.out.println("Usage: rpcregistry port")
     }
 }
