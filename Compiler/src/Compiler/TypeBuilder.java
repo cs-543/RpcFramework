56af234ce5069c8374ff4193eda7980fee993a8a
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-12 00:23:18 +0900

    Introduce TypeBuilder for common generic types

---
+++ Compiler/src/Compiler/TypeBuilder.java
@@ -0,0 +1,50 @@
+package Compiler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TypeBuilder {
+    /**
+     * Maps primitive types to their wrapping type.
+     */
+    private static final Map<String, String> wrappers = new HashMap<String, String>();
+
+    static {
+        wrappers.put("boolean", "Boolean");
+        wrappers.put("byte", "Byte");
+        wrappers.put("char", "Character");
+        wrappers.put("double", "Double");
+        wrappers.put("float", "Float");
+        wrappers.put("int", "Integer");
+        wrappers.put("long", "Long");
+        wrappers.put("short", "Short");
+        wrappers.put("void", "Void");
+    }
+
+    private TypeBuilder() {
+    }
+
+    public static String genericCallbackType(String type) {
+        return genericType("Callback", type);
+    }
+
+    public static String genericOutType(String type) {
+        return genericType("Out", type);
+    }
+
+    public static String genericInOutType(String type) {
+        return genericType("InOut", type);
+    }
+
+    private static String genericType(String generic, String parameter) {
+        return generic + "<" + wrapperOf(parameter) + ">";
+    }
+
+    private static String wrapperOf(String type) {
+        if (wrappers.containsKey(type)) {
+            return wrappers.get(type);
+        }
+
+        return type;
+    }
+}
