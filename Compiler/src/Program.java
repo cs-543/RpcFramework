f624b8540b162693e6b72f8b9dab1d11bf3f7641
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-12 18:14:39 +0900

    Minor fix

--- Compiler/src/Program.java
+++ Compiler/src/Program.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class Program {
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
-            printUsage();
+            System.out.println("Usage: compiler <path>");
             return;
         }
 
@@ -17,18 +17,8 @@ public class Program {
 
         File interfaceFile = new File(idlFile.getParentFile(), interface_.getName() + ".java");
         File stubFile = new File(idlFile.getParentFile(), interface_.getName() + "_Stub.java");
-
-
-        /*
-        System.out.println("Parsed as:");
-        new InterfaceEmitter(System.out).emit(interface_);
-        */
-
+        
         new JavaInterfaceEmitter(new FileOutputStream(interfaceFile)).emit(interface_);
         new JavaStubEmitter(new FileOutputStream(stubFile)).emit(interface_);
-    }
-
-    private static void printUsage() {
-        System.out.println("Usage: compiler <path>");
     }
 }
