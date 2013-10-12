a57d013883f213f0668cb1f49a740be57bd8cca6
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-12 17:50:26 +0900

    Compiler takes output path as argument

--- Compiler/src/Program.java
+++ Compiler/src/Program.java
@@ -1,23 +1,34 @@
 import Compiler.*;
 
 import java.io.FileInputStream;
+import java.io.*;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        IdlCompiler compiler = new IdlCompiler(new FileInputStream("interface.idl"));
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+        File idlFile = new File(args[0]);
+
+        IdlCompiler compiler = new IdlCompiler(new FileInputStream(idlFile));
         Interface interface_ = compiler.getInterface();
 
+        File interfaceFile = new File(idlFile.getParentFile(), interface_.getName() + ".java");
+        File stubFile = new File(idlFile.getParentFile(), interface_.getName() + "_Stub.java");
+
+
+        /*
         System.out.println("Parsed as:");
         new InterfaceEmitter(System.out).emit(interface_);
+        */
 
-        System.out.print("\n--------------------------------\n");
+        new JavaInterfaceEmitter(new FileOutputStream(interfaceFile)).emit(interface_);
+        new JavaStubEmitter(new FileOutputStream(stubFile)).emit(interface_);
+    }
 
-        System.out.println("Java Interface:");
-        new JavaInterfaceEmitter(System.out).emit(interface_);
-
-        System.out.print("\n--------------------------------\n");
-
-        System.out.println("Java Stub:");
-        new JavaStubEmitter(System.out).emit(interface_);
+    private static void printUsage() {
+        System.out.println("Usage: compiler <path>");
     }
 }
