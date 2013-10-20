7f69f4327a5eb75df605bd7aed40619aff92109c
Yannick Le Pennec <yannick.lepennec@gmail.com>
2013-10-20 14:41:05 +0900

    Update Stub invoke and invokeAsync signatures

--- Rpc/src/Rpc/Stub.java
+++ Rpc/src/Rpc/Stub.java
@@ -1,11 +1,16 @@
 package Rpc;
 
 public abstract class Stub {
-    public <T> T invoke(String methodName, Object... arguments) throws Exception {
+    public Stub() {
+
+    }
+
+    public <T> T invoke(Call call) throws Exception {
+
         throw new Exception("Not implemented");
     }
 
-    public <T> void invokeAsync(Callback<T> callback, String methodName, Object... arguments) throws Exception {
+    public <T> void invokeAsync(Call call, Callback<T> callback) throws Exception {
         throw new Exception("Not implemented");
     }
 }
